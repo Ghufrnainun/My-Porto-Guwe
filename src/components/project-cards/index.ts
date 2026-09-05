58c64c4c06d56ffe51e51948b25e21f2928d59c0
Ghufron Ainun Najib <ghufrnainunajib@gmail.com>
2026-09-05 15:59:33 +0700

    fix project-cards index exports

--- src/components/project-cards/index.ts
+++ src/components/project-cards/index.ts
@@ -4,3 +4,4 @@ export { ProjectCardVariant1 } from './ProjectCardVariant1';
 export { ProjectCardVariant2 } from './ProjectCardVariant2';
 export { ProjectCardVariant3 } from './ProjectCardVariant3';
 export { UniversalProjectCard } from './UniversalProjectCard';
+
